b9ec100644ea42f03f6ad09061d9fb4efc275a2c
Subhendra Basu <subhendra.one@gmail.com>
2024-01-17 09:12:59 +0530

    code

--- intfact.py
+++ intfact.py
@@ -1,43 +1,121 @@
 import sys
-from mpmath import mp
-from mpmath import zetazero
 from primes import primes
 
-PREC=1024
-def get_zero(i):
-    global PREC
-    mp.prec = PREC
-    mp.dps = PREC
-    zero = str(zetazero(i).imag)
-    idx = zero.index(".")
-    zero = zero[idx + 1:]
-    return zero
-
-if __name__ == "__main__":
-    num = str(sys.argv[1])
-    c = 0
-    l = len(num)
-    nn = num[c % l]
-    zero_index = 1
-    zero = get_zero(zero_index)
-    pos = 0
+def define(num):
     f = open("./pi.txt","r")
     f.read(2)
     g = open("./e.txt","r")
     g.read(2)
-    count = 0
-    while True:
+    l = len(num)
+    probable_next_states = []
+    probable_next_indices = []
+    length_increment = []
+    num2 = num + num
+    l2 = len(num2)
+    c = 0
+    last_prime_index = -1 
+    n1 = ""
+    n2 = ""
+    if l2 - c > 2:
+        n1 = num2[c:(c+3)]
+    if l2 - c > 1:
+        n2 = num2[c:(c+2)]
+    if l2 - c > 0:
+        n3 = num2[c:(c+1)]
+        if int(n3) == 0:
+            n3 = 10
+    if n1 != "" and (last_prime_index + int(n1) + 1 < 167 or last_prime_index - int(n1) - 1 >= 0):
+        if last_prime_index + int(n1) + 1< 167:
+            probable_next_states.append(primes[last_prime_index+int(n1) + 1])
+            probable_next_indices.append(last_prime_index + int(n1)+ 1)
+            length_increment.append(3)
+        if last_prime_index - int(n1) - 1 >= 0:
+            probable_next_states.append(primes[last_prime_index-int(n1)-1])
+            probable_next_indices.append(last_prime_index - int(n1)-1)
+            length_increment.append(3)
+    if n2 != "" and (last_prime_index + int(n2) + 1 < 167 or last_prime_index - int(n2) - 1 >= 0):
+        if last_prime_index + int(n2) + 1 < 167:
+            probable_next_states.append(primes[last_prime_index + int(n2) + 1])
+            probable_next_indices.append(last_prime_index + int(n2)+ 1)
+            length_increment.append(2)
+        if last_prime_index - int(n2) - 1 >= 0:
+            probable_next_states.append(primes[last_prime_index - int(n2) - 1])
+            probable_next_indices.append(last_prime_index - int(n2) - 1)
+            length_increment.append(2)
+    if last_prime_index + int(n3) + 1 < 167 or last_prime_index - int(n3) - 1 >= 0:
+        if last_prime_index + int(n3) + 1 < 167:
+            probable_next_states.append(primes[last_prime_index+int(n3) + 1])
+            probable_next_indices.append(last_prime_index + int(n3) + 1)
+            length_increment.append(1)
+        if last_prime_index - int(n3) >= 0:
+            probable_next_states.append(primes[last_prime_index-int(n3) - 1])
+            probable_next_indices.append(last_prime_index - int(n3) - 1)
+            length_increment.append(1)
+    pos = 0
+    states = []
+    while c < l2:
+        print(c, l2)
         pp = f.read(1)
-        nn = num[c % l]
-        c = c + 1
         ee = g.read(1)
-        ss = pp + nn + ee
-        zz = zero[pos]
         pos = pos + 1
-        if int(ss) in primes and zz == '0':
-            input([ss, zz])
-            zero_index = zero_index + 1
-            zero = get_zero(zero_index)
-            pos = 0
-    f.close()
-    g.close()
+        for x in range(0, 10):
+            ss = pp + str(x) + ee
+            if int(ss) in primes and int(ss) in probable_next_states:
+                states.append(x)
+                input(ss)
+                idx = probable_next_states.index(int(ss))
+                last_prime_index = probable_next_indices[idx]
+                c = c + length_increment[idx]
+                if c == l2 - 1:
+                    break
+                probable_next_indices = []
+                probable_next_states = []
+                length_increment = []
+                n1 = ""
+                n2 = ""
+                n3 = ""
+                if l2 - c > 2:
+                    n1 = num2[c:(c+3)]
+                if l2 - c > 1:
+                    n2 = num2[c:(c+2)]
+                if l2 - c > 0:
+                    n3 = num2[c:(c+1)]
+                    if int(n3) == 0:
+                        n3 = 10
+                else:
+                    break
+                input([n1, n2, n3])
+                if n1 != "" and (last_prime_index + int(n1) + 1 < 167 or last_prime_index - int(n1) - 1 >= 0):
+                    if last_prime_index + int(n1) + 1 < 167:
+                        probable_next_states.append(primes[last_prime_index+int(n1) + 1])
+                        probable_next_indices.append(last_prime_index + int(n1) + 1)
+                        length_increment.append(3)
+                    if last_prime_index - int(n1) - 1 >= 0:
+                        probable_next_states.append(primes[last_prime_index-int(n1) - 1])
+                        probable_next_indices.append(last_prime_index - int(n1) - 1)
+                        length_increment.append(3)
+                if n2 != "" and (last_prime_index + int(n2) + 1 < 167 or last_prime_index - int(n2) - 1 >= 0):
+                    if last_prime_index + int(n2) + 1 < 167:
+                        probable_next_states.append(primes[last_prime_index + int(n2) + 1])
+                        probable_next_indices.append(last_prime_index + int(n2) + 1)
+                        length_increment.append(2)
+                    if last_prime_index - int(n2) - 1 >= 0:
+                        probable_next_states.append(primes[last_prime_index - int(n2) - 1])
+                        probable_next_indices.append(last_prime_index - int(n2) - 1) 
+                        length_increment.append(2)
+                if n3 != "" and (last_prime_index + int(n3) + 1 < 167 or last_prime_index - int(n3) - 1 >= 0):
+                    if last_prime_index + int(n3) + 1 < 167:
+                        probable_next_states.append(primes[last_prime_index+int(n3) + 1])
+                        probable_next_indices.append(last_prime_index + int(n3) + 1)
+                        length_increment.append(1)
+                    if last_prime_index - int(n3) - 1 >= 0:
+                        probable_next_states.append(primes[last_prime_index-int(n3) - 1])
+                        probable_next_indices.append(last_prime_index - int(n3) - 1)
+                        length_increment.append(1)
+    return states, pos
+    
+if __name__ == "__main__":
+    num = str(sys.argv[1])
+    states, size = define(num)
+    print(states, size)
+    #factor = factorize(states, size, num)
